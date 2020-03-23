de2179dcf08c29137949a1d22d267ddd5a0ca560
UrbanRoland <urolir@gmail.com>
2020-03-23 17:50:19 +0100

    feat: bejelentekző és regisztrációs oldal implementálása

---
+++ CRM/src/main/java/com/crm/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.crm.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/")
+	public String login(){
+		return "index";
+	}
+	
+	@RequestMapping("registration")
+	public String registration(){
+		return "registration";
+	}
+	
+
+	
+	
+	
+}
