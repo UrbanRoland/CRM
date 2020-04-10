d959522cfb82ae7025732ca82e32e4e87178cb3f
Urban Roland <h776775@stud.u-szeged.hu>
2020-04-10 11:19:56 +0200

    feat: regisztrációs oldal tovább fejlesztése

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,4 @@
+
 (function ($) {
     "use strict";
 
@@ -5,8 +6,7 @@
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
-
-
+    var input2 = $('.validate-secondMessage .wrap-input100 .input100');
     $('.validate-form').on('submit',function(){
         var check = true;
 
@@ -30,7 +30,7 @@
     function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            	return false;
+                return false;
             }
         }
         else {
@@ -39,23 +39,40 @@
             }
         }
     }
-    
-   
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
 
         $(thisAlert).addClass('alert-validate');
-        
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
 
         $(thisAlert).removeClass('alert-validate');
-     
     }
-  
     
+    $('.validate-form').on('submit',function(){
+    	  //  $("button").click(function() {
+    	       var check = true;
+
+    	       $('.validate-secondMessage .wrap-input100').attr("data-validate","Jelszó kitöltése kötelező!");
+    	        if($('#reg1').val()!=$('#reg2').val()){
+    	        	 
+    	     
+    	        		$('.validate-secondMessage .wrap-input100').attr("data-validate","A két jelszó nem egyezzik meg!");
+    	 	
+    	        	 for(var i=0; i<input2.length; i++) {
+    	                 
+    	                     showValidate(input2[i]);
+    	                     check=false;
+    	                 
+    	             }
+    	
+    	        	return check;
+    	       
+    	        }
+    	      
+    	    });
 
 })(jQuery);
